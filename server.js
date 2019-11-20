968123339603e4bc6f79dddfa9313d31da2da216
someone <someone@someplace.com>
2019-11-20 14:47:40 +0200

    Removed wrong route

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
 	console.log(`server started on ${PORT} `);
 });
